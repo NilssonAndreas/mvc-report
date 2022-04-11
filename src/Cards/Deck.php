fd91e4a5e7110e2bacd172744003249fdae4b0b9
Andreas <annt21@student.bth.se>
2022-04-11 13:30:32 +0100

    added shuffle deck

--- src/Cards/Deck.php
+++ src/Cards/Deck.php
@@ -9,10 +9,23 @@ class Deck
     public function __construct()
     {
         $this->deck = new \App\Cards\Card();
+        $this->deck = $this->deck->getSet();
     }
 
     public function get(): array
     {
-        return $this->deck->getSet();
+        return $this->deck;
+    }
+
+    public function shuffleDeck(): array
+    {
+        shuffle($this->deck);
+        return $this->deck;
+    }
+
+    public function reset(): array
+    {
+        $this->deck = new \App\Cards\Card();
+        $this->deck = $this->deck->getSet();
     }
 }
--- src/Controller/card/CardController.php
+++ src/Controller/card/CardController.php
@@ -27,9 +27,24 @@ class CardController extends AbstractController
     /**
      * @Route("/card/deck", name="card-deck")
      */
-    public function roll(): Response
+    public function deck(): Response
     {
         $deck = new \App\Cards\Deck();
+        $data = [
+            'title' => 'Current-Deck',
+            'deck' => $deck->get()
+        ];
+        return $this->render('card/card.html.twig', $data);
+    }
+
+    /**
+     * @Route("/card/deck/shuffle", name="deck-shuffle")
+     */
+    public function shuffle(): Response
+    {
+        $deck = new \App\Cards\Deck();
+        $deck->shuffleDeck();
+        $shuffle = $deck->get();
         $data = [
             'title' => 'Current-Deck',
             'deck' => $deck->get()
