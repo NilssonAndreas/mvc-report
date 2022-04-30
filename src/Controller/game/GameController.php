cce7c61fb250192a994515bce3f76c1aac40e5d0
Andreas <annt21@student.bth.se>
2022-04-30 12:37:07 +0100

    Added Game controller and twig

---
+++ src/Controller/game/GameController.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Controller\game;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GameController extends AbstractController
+{
+    /**
+     * @Route(
+     *      "/game/",
+     *      name="game-home")
+     */
+    public function home(): Response
+    {
+        $data = [
+            'title' => 'Game'
+        ];
+        return $this->render('game/home.html.twig', $data);
+    }
+
+    /**
+     * @Route("/game/start", name="game-start")
+     */
+    public function start(): Response
+    {
+        // $deck = new \App\Cards\Deck();
+        $data = [
+            'title' => 'Game-Start',
+            // 'deck' => $deck->get()
+        ];
+        return $this->render('game/start.html.twig', $data);
+    }
+
+    // /**
+    //  * @Route("/card/deck/shuffle", name="deck-shuffle")
+    //  */
+    // public function shuffle(SessionInterface $session): Response
+    // {
+    //     $deck = new \App\Cards\Deck();
+    //     $deck->shuffleDeck();
+    //     $shuffle = $deck->get();
+    //     $data = [
+    //         'title' => 'Current-Deck',
+    //         'deck' => $deck->get()
+    //     ];
+    //     $session->set("myDeck", $deck);
+    //     return $this->render('card/card.html.twig', $data);
+    // }
+
+    // /**
+    // * @Route(
+    // * "/card/deck/draw",
+    // *  name="deck-draw")
+    // */
+    // public function draw(SessionInterface $session): Response
+    // {
+    //     $deck = $session->get("myDeck") ?? new \App\Cards\Deck();
+    //     $card = $deck->draw();
+    //     $count = $deck->countCards();
+    //     $data = [
+    //         'title' => 'Your Draw',
+    //         'draw' => $card,
+    //         'count' => $count
+    //     ];
+    //     $session->set("myDeck", $deck);
+    //     return $this->render('card/draw.html.twig', $data);
+    // }
+
+    // /**
+    //  * @Route("/card/deck/draw/{numRolls}", name="deck-multiRoll")
+    //  */
+    // public function multiDraw(int $numRolls, SessionInterface $session): Response
+    // {
+    //     $deck = $session->get("myDeck") ?? new \App\Cards\Deck();
+
+    //     $draw = [];
+    //     for ($i = 1; $i <= $numRolls; $i++) {
+    //         $draw[] = $deck->draw();
+    //     }
+    //     $count = $deck->countCards();
+    //     $data = [
+    //         'title' => 'This is your draw',
+    //         'numRolls' => $numRolls,
+    //         'draw' => $draw,
+    //         'count' => $count
+    //     ];
+    //     $session->set("myDeck", $deck);
+    //     return $this->render('card/draw.html.twig', $data);
+    // }
+
+    // /**
+    //  * @Route(
+    //  * "/card/deck/deal/{numPlayer}/{numCards}",
+    //  *  name="deck-deal")
+    //  * )
+    //  */
+    // public function playerDeal(int $numPlayer, int $numCards): Response
+    // {
+    //     $deck = new \App\Cards\Deck();
+    //     $deck->shuffleDeck();
+    //     $players = [];
+    //     for ($i = 1; $i <= $numPlayer; $i++) {
+    //         $newPlayer = new \App\Cards\Player();
+    //         $cards = [];
+    //         for ($x = 1; $x <= $numCards; $x++) {
+    //             $cards[] = $deck->draw();
+    //         }
+    //         $newPlayer->addCards($cards);
+    //         $players[] = [
+    //             'name' => "Player: {$i}",
+    //             'cards' => $newPlayer->getCards()
+    //         ];
+    //     }
+
+    //     $count = $deck->countCards();
+    //     $data = [
+    //         'title' => 'Deal',
+    //         'numPlayer' => $numPlayer,
+    //         'numCards' => $numCards,
+    //         'players' => $players,
+    //         'count' => $count
+    //     ];
+
+    //     return $this->render('card/deal.html.twig', $data);
+    // }
+
+    // /**
+    //  * @Route("/card/deck2", name="card-deck2")
+    //  */
+    // public function deck2(): Response
+    // {
+    //     $deck = new \App\Cards\DeckWith2Jokers();
+    //     $data = [
+    //         'title' => 'Joker',
+    //         'deck' => $deck->get()
+    //     ];
+    //     return $this->render('card/card.html.twig', $data);
+    // }
+}
